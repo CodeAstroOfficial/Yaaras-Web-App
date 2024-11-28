242723b671ad9c024a86e19469fe1d62c9c28ced
talhazaryabDA6~~ <zaryabdev404@gmail.com>
2024-11-28 11:02:56 +0500

    Removed Auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useAuthenticator } from '@aws-amplify/ui-react';
+
 import { generateClient } from "aws-amplify/data";
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
@@ -7,7 +7,7 @@ const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const { signOut } = useAuthenticator();
+
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
@@ -34,14 +34,6 @@ function App() {
             key={todo.id}>{todo.content}</li>
         ))}
       </ul>
-      <button onClick={signOut}>Sign out</button>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,3 @@
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from "aws-amplify";
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -11,8 +9,8 @@ Amplify.configure(outputs);
 ReactDOM.createRoot(document.getElementById("root")!).render(
 
   <React.StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
+
+    <App />
+
   </React.StrictMode>
 );
