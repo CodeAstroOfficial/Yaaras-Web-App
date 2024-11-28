a450bf8eefe5d84ded10bde890132993a4d10b77
talhazaryabDA6~~ <zaryabdev404@gmail.com>
2024-11-28 20:25:01 +0500

    Updated UI

--- src/pages/Clients/Clients.tsx
+++ src/pages/Clients/Clients.tsx
@@ -1,17 +1,17 @@
-// import ClientDetails from './ClientDetails'
-import ClientListing from './ClientListing'
+import ClientDetails from "./ClientDetails";
+import ClientListing from "./ClientListing";
 
 const Clients = () => {
-    return (
-        <div className="h-[calc(100vh-4rem)]  flex overflow-y-hidden">
-            <div className="h-full w-64  ">
-                <ClientListing />
-            </div>
-            <div className="flex flex-col flex-1 flex-shrink min-w-0 overflow-y-scroll">
-                {/* <ClientDetails /> */}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="h-[calc(100vh-4rem)]  lg:h-screen  flex overflow-y-hidden">
+      <div className="h-full w-64  ">
+        <ClientListing />
+      </div>
+      <div className="flex flex-col flex-1 flex-shrink min-w-0 overflow-y-scroll">
+        <ClientDetails />
+      </div>
+    </div>
+  );
+};
 
-export default Clients
+export default Clients;
